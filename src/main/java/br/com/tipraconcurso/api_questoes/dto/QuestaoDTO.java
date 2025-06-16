5670fd3e3479071a9be75de0f74d9e3cf4f23259
leonflicts42 <leonflicts.42@gmail.com>
2025-06-16 19:53:55 -0300

    add QuestaoDTO to dto

---
+++ src/main/java/br/com/tipraconcurso/api_questoes/dto/QuestaoDTO.java
@@ -0,0 +1,24 @@
+package br.com.tipraconcurso.api_questoes.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class QuestaoDTO {
+
+    private Long id;
+    private String banca;
+    private String orgao;
+    private String ano;
+    private String enunciado;
+    private String alternativaA;
+    private String alternativaB;
+    private String alternativaC;
+    private String alternativaD;
+    private String alternativaE;
+    private char gabarito;
+
+}
